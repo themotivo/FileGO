cd25be4ffeecf09cab9ec3b3c1eb07fde1c1a17c
Ladislau Gloria <l.gloria@motivo.io>
2018-04-03 18:46:40 +0100

    Changing the welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -71,7 +71,7 @@
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
-                        <a href="{{ route('login') }}">Loginsssss</a>
+                        <a href="{{ route('login') }}">Loginssssss</a>
                         <a href="{{ route('register') }}">Register</a>
                     @endauth
                 </div>
