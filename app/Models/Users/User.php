fcd4fe4ce60f6380aa564fff4c9a2a3073a196e4
Ladislau Gloria <l.gloria@motivo.io>
2018-04-03 22:21:27 +0100

    Additional things done.

--- app/Models/Users/User.php
+++ app/Models/Users/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Models\Users;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -15,7 +15,10 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+
+        'name',
+        'email',
+        'password'
     ];
 
     /**
@@ -24,6 +27,8 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+
+        'password',
+        'remember_token'
     ];
 }
