b5e1ebc49dd286a9fb8de77b5012da649f968b18
Ladislau Gloria <l.gloria@motivo.io>
2018-04-04 00:56:41 +0100

    More...

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,17 @@
 | Authentication Routes
 |--------------------------------------------------------------------------
 */
-Auth::routes();
+Route::get('/', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('/', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
 /*
 |--------------------------------------------------------------------------
 | Application Routes
 |--------------------------------------------------------------------------
 */
-Route::get('/', 'Application\LoginController@index');
 Route::get('/home', 'HomeController@index')->name('home');
