b9e233ec041c9826cfd36729e02f5e9e27c4f7ce
Ladislau Gloria <l.gloria@motivo.io>
2018-04-04 09:12:32 +0100

    New updates

--- routes/web.php
+++ routes/web.php
@@ -5,17 +5,17 @@
 | Authentication Routes
 |--------------------------------------------------------------------------
 */
-Route::get('/', 'Auth\LoginController@showLoginForm')->name('login');
-Route::post('/', 'Auth\LoginController@login');
-Route::post('logout', 'Auth\LoginController@logout')->name('logout');
-Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
-Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
-Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm');
-Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+Route::get('/',                         'Auth\LoginController@showLoginForm')->name('login');
+Route::post('/',                        'Auth\LoginController@login');
+Route::post('logout',                   'Auth\LoginController@logout')->name('logout');
+Route::get('password/reset',            'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email',           'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}',    'Auth\ResetPasswordController@showResetForm');
+Route::post('password/reset',           'Auth\ResetPasswordController@reset');
 
 /*
 |--------------------------------------------------------------------------
 | Application Routes
 |--------------------------------------------------------------------------
 */
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home',                     'HomeController@index')->name('home');
